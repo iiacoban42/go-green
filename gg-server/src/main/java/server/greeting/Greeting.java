8d37ec840e538177738fefd0f583f6b9d5f9da89
ricovanleeuwen <r.vanleeuwen-3@student.tudelft.nl>
2019-02-19 16:14:44 +0000

    Added jersey dependencies and example implementation

---
+++ gg-server/src/main/java/server/greeting/Greeting.java
@@ -0,0 +1,21 @@
+package server.greeting;
+
+public class Greeting {
+    private String message;
+
+    public Greeting(String name) {
+        this.message = getGreeting(name);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String name) {
+        this.message = name;
+    }
+
+    private String getGreeting(String name) {
+        return "Hello " + name;
+    }
+}
