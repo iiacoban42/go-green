e012f62525b59dfdf5ca0cf9b0049ed2ac180afd
Eliott Afriat <eliott@afriat.net>
2019-02-19 17:34:33 +0100

    Might have a working databse now

--- src/main/server/com/hib/entities/User.java
+++ src/main/server/com/hib/entities/User.java
@@ -1,4 +1,38 @@
 package com.hib.entities;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class Task {
+    @Id
+    @Column(name="username")
+    private String username;
+
+    @Column(name="hash_password")
+    private String hashPassword;
+
+    Public User(String username, String hashPassword){
+        this.username = username;
+        this.hashPassword = hashPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
 }
