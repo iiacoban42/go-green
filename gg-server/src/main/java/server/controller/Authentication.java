fa991d0c97ca769bccc285fd9a63d6b6d0b617a3
ricovanleeuwen <r.vanleeuwen-3@student.tudelft.nl>
2019-02-28 21:27:41 +0100

    Added server tests

---
+++ gg-server/src/main/java/server/controller/Authentication.java
@@ -0,0 +1,51 @@
+package server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authentication")
+public class Authentication {
+
+    /**
+     * Check if login credentials are valid.
+     * @param credentials to check if valid
+     * @return true or false
+     */
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginCredentials credentials) {
+        LoginCredentials login = new LoginCredentials("user", "pass");
+
+        ResponseEntity response = new ResponseEntity(HttpStatus.UNAUTHORIZED);
+
+        if (credentials.getUsername().equals("admin")) {
+            response = new ResponseEntity(HttpStatus.OK);
+        }
+
+        return response;
+    }
+
+    /**
+     * Takes credentials and returns true or false I guess.
+     * @param credentials to check if valid
+     * @return boolean true or false
+     */
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterCredentials credentials) {
+        RegisterCredentials register = new RegisterCredentials("mail", "user", "pass");
+        ResponseEntity response = new ResponseEntity(HttpStatus.OK);
+
+        System.out.println("hello world!");
+        System.out.println(credentials.getUsername());
+
+        if (credentials.getUsername().equals("admin")) {
+            response = new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
+        return response;
+    }
+}
