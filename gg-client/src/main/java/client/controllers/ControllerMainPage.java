62ec868cdee3bf22e00631f8b292be30265c1266
Ioanna <i.nika@student.tudelft.nl>
2019-03-03 20:48:01 +0100

    GUI components - add FXML annotations-controller

--- gg-client/src/main/java/client/controllers/ControllerMainPage.java
+++ gg-client/src/main/java/client/controllers/ControllerMainPage.java
@@ -1,4 +1,41 @@
 package client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControllerMainPage {
+
+    @FXML
+    Button logoutButton;
+
+    @FXML
+    Button vegieButton;
+
+    @FXML
+    Button transportButton;
+
+    @FXML
+    Button temperatureButton;
+
+    @FXML
+    Button localProduceButton;
+
+    @FXML
+    Button solarButton;
+
+    @FXML
+    Button bikeButton;
+
+    @FXML
+    Button leaderboardButton;
+
+    @FXML
+    Button savedcoButton;
+
+    @FXML
+    Button friendsButton;
+
+    @FXML
+    Button achievementsButton;
+
 }
