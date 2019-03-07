ed05d3bdfbe9d555a70429f362d265ead1fa769f
iiacoban <i.iacoban@student.tudelft.nl>
2019-03-07 16:17:58 +0100

    Change structure of Meal object

--- src/main/java/Features/Meal.java
+++ src/main/java/Features/Meal.java
@@ -3,10 +3,14 @@ package Features;
 public class Meal {
     private String product;
     private double co2;
+    private double quantity;
+    private boolean selected;
 
-    public Meal(String product, double co2) {
+    public Meal(String product, double co2, double quantity, boolean selected) {
         this.product = product;
         this.co2 = co2;
+        this.quantity = quantity;
+        this.selected = selected;
     }
 
     public String getProduct() {
@@ -23,5 +27,21 @@ public class Meal {
 
     public void setCo2(double co2) {
         this.co2 = co2;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
     }
 }
--- src/main/java/Features/VeggieMeal.java
+++ src/main/java/Features/VeggieMeal.java
@@ -3,61 +3,31 @@ package Features;
 import java.util.ArrayList;
 
 public class VeggieMeal {
-    private String name;
-    private int weight;
-    private double co2;
+    private ArrayList<Meal> meal;
 
-
-    public VeggieMeal(String name, int weight, float co2) {
-        this.name = name;
-        this.weight = weight;
-        this.co2 = co2;
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public double getCo2() {
-        return co2;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public void setCo2(float co2) {
-        this.co2 = co2;
+    public VeggieMeal(ArrayList<Meal> meal) {
+        meal = new ArrayList<Meal>();
     }
 
     public static ArrayList<Meal> menu() {
         ArrayList<Meal> menu = new ArrayList<Meal>();
-        menu.add(new Meal("beans", 0.001));
-        menu.add(new Meal("veggie burger", 0.0026));
-        menu.add(new Meal("insects", 0.0027));
-        menu.add(new Meal("quorn", 0.0027));
-        menu.add(new Meal("nuts", 0.0032));
-        menu.add(new Meal("tofu", 0.0035));
-        menu.add(new Meal("egg", 0.216));
-        menu.add(new Meal("beef croquette", 0.0052));
-        menu.add(new Meal("veggie burger + cheese", 0.0065));
-        menu.add(new Meal("chicken", 0.0068));
-        menu.add(new Meal("pork", 0.0070));
-        menu.add(new Meal("cheese", 0.0100));
-        menu.add(new Meal("mixed minced meat", 0.0133));
-        menu.add(new Meal("hamburger", 0.0168));
-        menu.add(new Meal("minced meat", 0.0194));
-        menu.add(new Meal("steak", 0.0340));
-        menu.add(new Meal("lamb", 0.0510));
+        menu.add(new Meal("beans", 0.001,0,false));
+        menu.add(new Meal("veggie burger", 0.0026,0,false));
+        menu.add(new Meal("insects", 0.0027,0,false));
+        menu.add(new Meal("quorn", 0.0027,0,false));
+        menu.add(new Meal("nuts", 0.0032,0,false));
+        menu.add(new Meal("tofu", 0.0035,0,false));
+        menu.add(new Meal("egg", 0.216,0,false));
+        menu.add(new Meal("beef croquette", 0.0052,0,false));
+        menu.add(new Meal("veggie burger + cheese", 0.0065,0,false));
+        menu.add(new Meal("chicken", 0.0068,0,false));
+        menu.add(new Meal("pork", 0.0070,0,false));
+        menu.add(new Meal("cheese", 0.0100,0,false));
+        menu.add(new Meal("mixed minced meat", 0.0133,0,false));
+        menu.add(new Meal("hamburger", 0.0168,0,false));
+        menu.add(new Meal("minced meat", 0.0194,0,false));
+        menu.add(new Meal("steak", 0.0340,0,false));
+        menu.add(new Meal("lamb", 0.0510,0,false));
 
         return menu;
     }
@@ -65,13 +35,18 @@ public class VeggieMeal {
     public static double calculator(ArrayList<Meal> selection) {
         double calculator = 0;
         for (Meal mealUser: selection) {
-            for (Meal mealMenu: menu()) {
-                if (mealUser.getProduct().equals(mealMenu.getProduct())) {
-                    calculator += mealMenu.getCo2() * mealUser.getCo2();
+                if (mealUser.isSelected()) {
+                    calculator += mealUser.getCo2() * mealUser.getQuantity();
                 }
             }
-        }
         return calculator;
     }
 
+    public static void main(String[] args) {
+        ArrayList<Meal> test = new ArrayList<Meal>(menu());
+        test.get(16).setSelected(true);
+        test.get(16).setQuantity(1000);
+        System.out.println(calculator(test));
+
+    }
 }
