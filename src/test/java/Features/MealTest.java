254fb9264acfbe1a670bf16db15930910f00373c
noekepoek8 <anoek.winkelman@live.nl>
2019-03-09 14:38:04 +0100

    Test Veggiemeal

--- src/test/java/Features/MealTest.java
+++ src/test/java/Features/MealTest.java
@@ -3,19 +3,19 @@ package Features;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MealTest {
     @Test
     public void mealTest() {
-    Meal mealTest = new Meal(null, 0, 0, false);
-    mealTest.setProduct("beans");
-    mealTest.setCo2(0.001);
+    Meal mealTest = new Meal("beans", 0.001, 0, true,false);
     mealTest.setQuantity(200);
     mealTest.setSelected(true);
     assertEquals("beans", mealTest.getProduct());
     assertEquals(0.001, mealTest.getCo2(), 0.0005);
     assertEquals(200, mealTest.getQuantity(), 5);
-    assertEquals(true, mealTest.isSelected());
+    assertTrue(mealTest.isVegetarian());
+    assertTrue(mealTest.isSelected());
     }
 
     @Test
@@ -24,8 +24,8 @@ public class MealTest {
         VeggieMeal.get(1).setSelected(true);
         VeggieMeal.get(5).setQuantity(100);
         VeggieMeal.get(5).setSelected(true);
-        VeggieMeal.get(6).setQuantity(1);
-        VeggieMeal.get(6).setSelected(true);
-        assertEquals(VeggieMeal.calculator());
+        VeggieMeal.get(7).setQuantity(1);
+        VeggieMeal.get(7).setSelected(true);
+        assertEquals(1.446, VeggieMeal.calculator(), 0.0005);
     }
 }
