d4aa51d971427a88fd97c85bdb5eb71846e17e10
iiacoban <i.iacoban@student.tudelft.nl>
2019-03-18 20:51:21 +0100

    Do checkstyle and test

--- gg-server/src/main/java/server/transportation/TransportationCalculator.java
+++ gg-server/src/main/java/server/transportation/TransportationCalculator.java
@@ -6,25 +6,31 @@ import server.entity.TransportList;
 /**
  * Reference to Co2 values.
  * https://www.delijn.be/en/overdelijn/organisatie/zorgzaam-ondernemen/milieu/co2-uitstoot-voertuigen.html
- * https://ecf.com/news-and-events/news/how-much-co2-does-cycling-really-save
- * https://loco2.com/en/blog/travel-green-calculating-your-carbon-savings
+ * bike:  https://ecf.com/news-and-events/news/how-much-co2-does-cycling-really-
  */
 
 public class TransportationCalculator {
-    private static double carCo2 = 127;
+
     public static final Transportation[] vehicle = {
-            new Transportation("bike", 21),
-            new Transportation("bus", 101),
-            new Transportation("tram", 23),
-            new Transportation("metro", 30.5),
-            new Transportation("train", 28),
-            new Transportation("scooter", 77)
+
+        new Transportation("bike", 5),
+        new Transportation("bus", 101),
+        new Transportation("tram", 23),
+        new Transportation("metro", 30.5),
+        new Transportation("train", 28),
+        new Transportation("scooter", 77)
     };
 
+    /**
+     * Calculates the Co2 saved by taking public transportation.
+     * @param transportList contains the means of transportation the user selected
+     * @return the Co2 saved compared to a car
+     */
     public static double getAmountCo2(TransportList transportList) {
         double co2 = 0;
         double result;
         double totalDistance = 0;
+        double carCo2 = 127;
         for (Transport listTransport : transportList.getTransport()) {
             for (Transportation transportation : vehicle) {
                 if (transportation.getName().equals(listTransport.getName())) {
@@ -33,6 +39,7 @@ public class TransportationCalculator {
                 }
             }
         }
+
         result = carCo2 * totalDistance - co2;
         return result;
     }
