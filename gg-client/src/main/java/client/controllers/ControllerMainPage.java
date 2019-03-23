800290e608be86c5899cf12ccd5cda0e68ac11b5
ricovanleeuwen <r.vanleeuwen-3@student.tudelft.nl>
2019-03-23 19:53:20 +0000

    Fix checkstyle

--- gg-client/src/main/java/client/controllers/ControllerMainPage.java
+++ gg-client/src/main/java/client/controllers/ControllerMainPage.java
@@ -3,12 +3,10 @@ package client.controllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
-import com.jfoenix.transitions.JFXFillTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -94,14 +92,15 @@ public class ControllerMainPage extends ControllerGeneral {
     @FXML
     public void homePageButtonPressed(ActionEvent event) throws IOException {
 
-        changeScene("mainMenu.fxml" ,anchorPane);
+        changeScene("mainMenu.fxml", anchorPane);
     }
 
     /**
-    * When bike button is pressed a new window appears.
-    * @param event bike button pressed.
-    * @throws IOException if something goes wrong.
-    */
+     * When bike button is pressed a new window appears.
+     *
+     * @param event bike button pressed.
+     * @throws IOException if something goes wrong.
+     */
 
     @FXML
     public void bikeButtonPressed(ActionEvent event) throws IOException {
@@ -112,14 +111,15 @@ public class ControllerMainPage extends ControllerGeneral {
 
 
     /**
-    * Change window.
-    * @param event button for public transportation pressed.
-    * @throws IOException if something goes wrong.
-    */
+     * Change window.
+     *
+     * @param event button for public transportation pressed.
+     * @throws IOException if something goes wrong.
+     */
 
 
     @FXML
-    public void publicTransportationPressed( ActionEvent event) throws IOException {
+    public void publicTransportationPressed(ActionEvent event) throws IOException {
 
         changeWindow("publicTransportation.fxml");
 
@@ -128,9 +128,8 @@ public class ControllerMainPage extends ControllerGeneral {
     @FXML
     public void logButtonPressed(ActionEvent event) throws IOException {
 
-        changeScene("log.fxml" , anchorPane );
+        changeScene("log.fxml", anchorPane);
     }
-
 
 
     /**
@@ -139,21 +138,21 @@ public class ControllerMainPage extends ControllerGeneral {
     @FXML
     public void loadMealDialog(MouseEvent event) {
 
-        JFXDialogLayout content =  new JFXDialogLayout();
+        JFXDialogLayout content = new JFXDialogLayout();
         content.setHeading(new Text("Meal Calculator"));
         content.setBody(new Text("We calculate the Carbon Dioxide\n"
-                +
-                                 "you produce by comparing your\n"
-                +
-                                 "meal with the Dutch average meal.\n\n"
-                +
+            +
+            "you produce by comparing your\n"
+            +
+            "meal with the Dutch average meal.\n\n"
+            +
 
-                "Please fill what you ate and then press Submit.\n"
-                ));
+            "Please fill what you ate and then press Submit.\n"
+        ));
 
         JFXButton button = new JFXButton("Close");
 
-        JFXDialog dialog = new JFXDialog(stackMeal , content , JFXDialog.DialogTransition.CENTER);
+        JFXDialog dialog = new JFXDialog(stackMeal, content, JFXDialog.DialogTransition.CENTER);
 
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -163,7 +162,7 @@ public class ControllerMainPage extends ControllerGeneral {
         });
 
         content.setActions(button);
-            dialog.show();
+        dialog.show();
     }
 
 }
