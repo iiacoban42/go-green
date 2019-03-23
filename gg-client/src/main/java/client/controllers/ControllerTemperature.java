9bdb63d241355df8df59546b11752c92145ddf5f
iiacoban <i.iacoban@student.tudelft.nl>
2019-03-23 16:01:19 +0100

    Add temperature gui

---
+++ gg-client/src/main/java/client/controllers/ControllerTemperature.java
@@ -0,0 +1,30 @@
+package client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ControllerTemperature {
+
+    @FXML
+    private TextField surface;
+
+    @FXML
+    private TextField energy;
+
+    @FXML
+    private RadioButton electricity;
+
+    @FXML
+    private RadioButton naturalGas;
+
+    @FXML
+    private RadioButton heatingOil;
+
+    @FXML
+    private Button submit;
+
+
+
+}
