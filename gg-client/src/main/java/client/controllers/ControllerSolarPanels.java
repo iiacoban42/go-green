c768d4339d24694889ac20615482c0bc4c55e26b
Ioanna <i.nika@student.tudelft.nl>
2019-03-24 17:16:04 +0100

    Fix main menu

--- gg-client/src/main/java/client/controllers/ControllerSolarPanels.java
+++ gg-client/src/main/java/client/controllers/ControllerSolarPanels.java
@@ -1,5 +1,27 @@
 package client.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
 public class ControllerSolarPanels {
+
+    @FXML
+    JFXButton submit;
+
+    @FXML
+    JFXTextField panels;
+
+    /**
+     * Close window.
+     * @param event submit pressed.
+     */
+    @FXML
+    public void submitButtonPressed(ActionEvent event) {
+        submit.getScene().getWindow().hide();
+
+    }
 
 }
