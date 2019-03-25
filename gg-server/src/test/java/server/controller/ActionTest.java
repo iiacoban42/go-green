dbae64dd5bb3ced0b64205928dc36cceacc0a90b
ricovanleeuwen <r.vanleeuwen-3@student.tudelft.nl>
2019-03-25 18:16:23 +0000

    Add cleanup after ActionController tests

--- gg-server/src/test/java/server/controller/ActionTest.java
+++ gg-server/src/test/java/server/controller/ActionTest.java
@@ -3,6 +3,7 @@ package server.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import database.manager.ActionManager;
 import database.manager.UserManager;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -33,6 +34,7 @@ public class ActionTest {
     private ObjectMapper objectMapper;
 
     @Before
+    @After
     public void deleteActions() {
         List actions = ActionManager.listActionsUser("admin");
 
