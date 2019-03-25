03dbe74d48441e2576f8865b218dafc7000fbc3d
ricovanleeuwen <r.vanleeuwen-3@student.tudelft.nl>
2019-03-25 18:12:11 +0000

    Add friend control to api including tests

--- gg-server/src/test/java/server/database/UserManagerTest.java
+++ gg-server/src/test/java/server/database/UserManagerTest.java
@@ -1,7 +1,5 @@
 package server.database;
 
-import org.hibernate.HibernateError;
-import org.hibernate.HibernateException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -89,7 +87,7 @@ public class UserManagerTest {
     }
 
     @Test
-    public void addFeind() {
+    public void addFriend() {
         UserManager.addFriend("cpt1", "cpt2");
         assertEquals("cpt2", UserManager.getUser("cpt1").getFriend());
         assertEquals("cpt1", UserManager.getUser("cpt2").getFriend());
