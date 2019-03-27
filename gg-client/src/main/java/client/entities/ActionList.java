87c74cf1b0215a119195ccc754c23bb0c22fab52
Ioanna <i.nika@student.tudelft.nl>
2019-03-27 22:00:21 +0100

    Fix List View

--- gg-client/src/main/java/client/entities/ActionList.java
+++ gg-client/src/main/java/client/entities/ActionList.java
@@ -2,6 +2,7 @@ package client.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ActionList {
 
@@ -46,13 +47,17 @@ public class ActionList {
 
         for (Action action : actionList) {
 
-            String actionDetails = action.getActionName() + "                   "
+            StringBuilder stringBuilder = new StringBuilder(50);
+            stringBuilder.insert(0 , action.getActionName() + "              ");
+            stringBuilder.insert(16 , action.getScore());
+            stringBuilder.delete(16 + String.valueOf(action.getScore()).length(), action.getActionName().length() + String.valueOf(action.getScore()).length() + 14);
+            System.out.println(stringBuilder.toString());
 
-                    +
-                      action.getScore();
-
+            String actionDetails = stringBuilder.toString();
             details.add(actionDetails);
         }
+
+        System.out.println( details.toString());
 
         return details;
     }
