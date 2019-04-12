cc1b5ea13b3ead64e9108f647f368ab26918701c
Ioanna <i.nika@student.tudelft.nl>
2019-04-12 23:56:24 +0200

    Fix pop up information boxes.

--- gg-client/src/main/java/client/controllers/ControllerSolarPanels.java
+++ gg-client/src/main/java/client/controllers/ControllerSolarPanels.java
@@ -1,12 +1,23 @@
 package client.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import org.springframework.web.client.RestClientResponseException;
+
+import java.awt.*;
+
+import static client.requests.SolarPanelRequests.getSolarPanels;
+import static client.requests.SolarPanelRequests.sendSolarPanels;
 
 
 public class ControllerSolarPanels {
+
+    @FXML
+    Label numSP;
 
     @FXML
     JFXButton submit;
@@ -15,13 +26,34 @@ public class ControllerSolarPanels {
     JFXTextField panels;
 
     /**
+     * Display number of solar panels.
+     */
+    public void initialize() {
+
+        int num;
+        try {
+             num = getSolarPanels();
+        } catch (RestClientResponseException e) {
+            num = 0;
+        }
+        numSP.setText(Integer.toString(num));
+    }
+
+    /**
      * Close window.
      * @param event submit pressed.
      */
     @FXML
     public void submitButtonPressed(ActionEvent event) {
+        int amount = Integer.parseInt(panels.getText());
+        try {
+            sendSolarPanels(amount);
+        } catch (JsonProcessingException e) {
+            System.out.print("something went wrong");
+        }
         submit.getScene().getWindow().hide();
 
     }
+
 
 }
